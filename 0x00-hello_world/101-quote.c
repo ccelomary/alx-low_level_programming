9728e7a782ebf3ae3cfbf4ab5969f0ac6d0f5394
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-02-16 14:23:52 +0100

    feat: finish adevance part

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 1 (Failure)
+ */
+int	main(void)
+{
+	const char *quote = "and that piece of art is useful\" - \
+			     Dora Korpar, 2015-10-19\n";
+	write(2, quote, sizeof(char) * strlen(quote));
+	return (1);
+}
