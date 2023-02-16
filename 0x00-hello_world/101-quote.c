e30ecd7f0d6def535b06f6651af0737ecb0d4a79
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-02-16 15:14:08 +0100

    chore: fix quote error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
  */
 int	main(void)
 {
-	const char *quote = "and that piece of art is useful\" - \
-			     Dora Korpar, 2015-10-19\n";
+	char *quote;
+
+	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, quote, sizeof(char) * strlen(quote));
 	return (1);
 }
