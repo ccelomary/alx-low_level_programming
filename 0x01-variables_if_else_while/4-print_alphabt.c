e9fa4858a4dc53d332c1f6ec95ba2285987cffe8
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-02-17 18:58:30 +0100

    chore: fix issue

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,13 @@ int	main(void)
 	int	i;
 	char	c;
 
-	i = 0;
-	while (i <= 25)
+	i = -1;
+	while (++i <= 25)
 	{
 		c = i + 97;
 		if (c == 'q' || c == 'e')
 			continue;
 		putchar(c);
-		i++;
 	}
 	putchar('\n');
 	return (0);
