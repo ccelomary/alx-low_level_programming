d9d8252c02b0246b5662a099e66107601c05bf1c
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-02-24 01:49:06 +0100

    chore: add main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int	_isupper(char);
+int	_isdigit(char);
+int	mul(int, int);
+int	_putchar(char);
+void	print_numbers(void);
+void	print_most_numbers(void);
+void	more_numbers(void);
+void	print_line(int);
+void	print_diagonal(int);
+void	print_square(int);
+void	print_triangle(int);
+#endif /* MAIN_H */
+
