7199563081bee29fab6faf3d8b6bf97b44e61561
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-03-09 17:56:00 +0100

    chore: fix function that print additional new line

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,26 +1,14 @@
 #include "main.h"
 /**
- * _print_rev_recursion_helper - is function that print string in reverse order
+ * _print_rev_recursion - function that print string in reverse order
  *
  * @s: string to be printed
  * Return: void
  */
-void _print_rev_recursion_helper(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (!*s)
 		return;
 	_print_rev_recursion_helper(++s);
 	_putchar(*s);
-}
-
-/**
- * _print_rev_recursion - function that print given string in reverse order
- *
- * @s: string to be printed
- * Return: void
- */
-void	_print_rev_recursion(char *s)
-{
-	_print_rev_recursion_helper(s);
-	_putchar('\n');
 }
