1f961151f9aaa5fc0ad6f5d3b694499430654a0c
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-03-30 17:23:12 +0000

    feat: fix issue concering adding node to list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,27 +3,27 @@
  * add_node - function that adds new node to the start of the llist
  *
  * @head: head of llist
- * @string: string to enter as data
+ * @str: string to enter as data
  * Return: new added node or null
  */
-list_t		*add_node(list_t **head, const char *string)
+list_t		*add_node(list_t **head, const char *str)
 {
 	list_t	*current_node;
-	char	*str;
+	char	*string;
 	size_t	slen;
 
-	slen = _strlen(string);
-	str = malloc(slen + 1);
-	if (!str)
+	slen = _strlen(str);
+	string = malloc(slen + 1);
+	if (!string)
 		return (NULL);
 	current_node = malloc(sizeof(list_t));
 	if (!current_node)
 	{
-		free(str);
+		free(string);
 		return (NULL);
 	}
 	current_node->len = slen;
-	current_node->str = _strcpy(str, string);
+	current_node->str = _strcpy(string, str);
 	current_node->next = *head;
 	*head = current_node;
 	return (*head);
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,28 +4,28 @@
  * add_node_end - function that adds node to end of the llist
  *
  * @head: head of the llist
- * @string: the content of the llist
+ * @str: the content of the llist
  * Return: new created node
  */
-list_t	*add_node_end(list_t **head, const char *string)
+list_t	*add_node_end(list_t **head, const char *str)
 {
 	list_t	*current_node;
 	list_t	*tmp;
-	char	*str;
+	char	*string;
 	size_t	slen;
 
-	slen = _strlen(string);
-	str = malloc(slen + 1);
-	if (!str)
+	slen = _strlen(str);
+	string = malloc(slen + 1);
+	if (!string)
 		return (NULL);
 	current_node = malloc(sizeof(list_t));
 	if (!current_node)
 	{
-		free(str);
+		free(string);
 		return (NULL);
 	}
 	current_node->len = slen;
-	current_node->str = _strcpy(str, string);
+	current_node->str = _strcpy(string, str);
 	current_node->next = NULL;
 	if (!*head)
 		*head = current_node;
