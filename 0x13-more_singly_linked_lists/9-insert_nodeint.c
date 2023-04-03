ab8ac3154921a5f8cc8c39892a0d90286cc403d1
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-04-03 20:52:12 +0000

    fix: issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -74,6 +74,8 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (node);
 	}
 	tmp = insert_at_index_helper(head, node, idx);
+	if (tmp)
+		return (node);
 	if (!tmp)
 		free(node);
 	return (NULL);
