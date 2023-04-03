4ca9ae35e5ae96981d049a62da374f5f0cd168cf
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-04-03 20:41:46 +0000

    fix: issue

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,12 @@ void	free_listint2(listint_t **head)
 {
 	listint_t	*tmp;
 
+	if (!head)
+		return;
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	head = NULL;
 }
