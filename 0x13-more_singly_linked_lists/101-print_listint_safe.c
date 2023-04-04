8fbfda49c923ee8bc81396f31a7b5138339fd2a0
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-04-04 21:17:15 +0000

    chore: safe print

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+int			where_cycle_end(const listint_t *head)
+{
+	int			current_length;
+	int			iter;
+	listint_t	*previous;
+	listint_t	*current;
+
+	if (!head || !head->next)
+		return (-1);
+	current_length = 1;
+	previous = (listint_t *)head;
+	current = head->next;
+	while (current)
+	{
+		previous = (listint_t *)head;
+		iter = 0;
+		while (iter < current_length)
+		{
+			if (previous == current)
+			{
+				return (current_length);
+			}
+			iter++;
+			previous = previous->next;
+		}
+		current_length++;
+		current = current->next;
+	}
+	return (-1);
+}
+/**
+ * print_listint_safe - function that prints what's (address, value) each node
+ *
+ * @head: head of the linked list
+ * Return: number of element printed
+ */
+size_t		print_listint_safe(const listint_t *head)
+{
+	int			iter;
+	size_t		printed_len;
+	int 		position;
+
+	iter = 0;
+	printed_len = 0;
+	position = where_cycle_end(head);
+	while (head)
+	{
+		if (iter == position)
+		{
+			printf("-> [%p] %d\n", head, head->n);
+			exit(98);
+		}
+		printf("[%p] %d\n", head, head->n);
+		printed_len++;
+		iter++;
+		head = head->next;
+	}
+	return (printed_len);
+}
