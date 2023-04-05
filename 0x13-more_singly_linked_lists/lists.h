e3180fc9e229ba89bbd747d4b4f3ef2aa70feb9a
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-04-05 02:48:13 +0000

    fix: list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,10 +18,10 @@ typedef struct	listint_s
 	struct listint_s	*next;
 }	listint_t;
 
-size_t		print_listint(const listint_t *);
-size_t		listint_len(const listint_t *);
-listint_t	*add_nodeint(listint_t **, const int );
-listint_t	*add_nodeint_end(listint_t **, const int);
+size_t		print_listint(const listint_t *h);
+size_t		listint_len(const listint_t *h);
+listint_t	*add_nodeint(listint_t **head, const int n);
+listint_t	*add_nodeint_end(listint_t **head, const int n);
 void		free_listint(listint_t *head);
 void		free_listint2(listint_t **head);
 int			pop_listint(listint_t **head);
