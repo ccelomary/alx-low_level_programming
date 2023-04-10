444169cefb31dbb0cc30e963a3372342ea52da56
Mohamed El omary <elomarymohamed72@gmail.com>
2023-04-10 16:45:10 +0000

    chore: add trunc flag

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,9 +33,7 @@ int		create_file(const char *filename, char *text_content)
 	int			fd;
 	ssize_t		number_of_written_letters;
 
-	if (filename == NULL)
-		return (FAIL);
-	fd = open(filename, O_WRONLY | O_CREAT, USER_RW);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, USER_RW);
 	if (fd == FILE_FAILURE)
 		return (FAIL);
 	number_of_written_letters = write(fd, text_content,
