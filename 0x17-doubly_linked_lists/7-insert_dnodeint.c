d8ceac0a15aa52c1995094338608e4df63d96bd1
Mohamed El omary <elomarymohamed72@gmail.com>
2023-06-15 16:41:53 +0100

    feat: fix the insertion to double linkedlist

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node = malloc(sizeof(dlistint_t));
 			if (!new_node)
 				return (NULL);
+			new_node->n = n;
 			new_node->next = tmp;
 			new_node->prev = tmp->prev;
 			tmp->prev->next = new_node;
