5996ba9ef64566a3ec7f195c9189716f0ec9ee16
Mohamed El omary <elomarymohamed72@gmail.com>
2023-07-09 17:41:37 +0100

    feat: hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -54,6 +54,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t **backet, *tmp;
 	char *val;
 
+	if (!key)
+		return (0);
 	index = key_index((unsigned const char *)key, ht->size);
 	backet = &ht->array[index];
 	tmp = *backet;
