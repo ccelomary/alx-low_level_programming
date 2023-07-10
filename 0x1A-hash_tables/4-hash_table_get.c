c1e4c034c84add9b87f94d3b170510bd55afd1ac
Mohamed El omary <elomarymohamed72@gmail.com>
2023-07-10 03:37:13 +0100

    feat: checker

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t const *node;
 
+	if (!ht || !key)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	while (node)
